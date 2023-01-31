bfe63126bf3fd92a5d4df262419ef06626a1423f
emilianomfortes@hotmail.com <emilianomfortes@hotmail.com>
2023-01-31 11:36:38 +0100

    Sample figure and improvements

--- src/mypy/plotters.py
+++ src/mypy/plotters.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot1d(data: (list or pd.DataFrame), ):
+def plot1d(data: (list or pd.DataFrame), x=None, y=None, figsize=(6,5), savefig=None, show=False):
+    
+    fig, ax = plt.subplots(figsize=figsize)
     
     if isinstance(data, pd.DataFrame):
         print("pd Plot")
+        if (x is not None) and (y is not None):
+            data.plot(x=x, y=y, ax=ax)
+        else:
+            raise ValueError("Tring to plot a pd.DataFrame without x and y col labels.")
     elif isinstance(data, list):
         if len(list) == 1:
             print("Only y data")
@@ -14,3 +20,10 @@ def plot1d(data: (list or pd.DataFrame), ):
             raise ValueError(f"data input is a list of a len different than 1 or 2.")
     else:
         raise ValueError(f"data is neither a pd.DataFrame or a list of np.arrays...")
+    
+    if savefig is not None:
+        fig.savefig(savefig)
+    if show:
+        plt.show()
+
+    return fig, ax
